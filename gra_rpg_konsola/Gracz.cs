38fcf205885b712cd1191a6b1074409d949464a5
szymonsowinski <sowinski.szymon@zstz-radzymin.pl>
2025-12-15 13:21:42 +0100

    update of gracz

--- gra_rpg_konsola/Gracz.cs
+++ gra_rpg_konsola/Gracz.cs
@@ -3,11 +3,24 @@ namespace gra_rpg_konsola
 {
 	public class Gracz
 	{
-		public int zdrowie, ;
+        public string Imie;
+
+        public float Zdrowie;
+        public float MaxZdrowie;
+
+        public float Stamina;
+        public float MaxStamina;
+
+		public float Obrazenia;
 
 
-		public Gracz()
+        public Gracz(float zdrowie, float stamina, float obrazenia)
 		{
+			this.Zdrowie = zdrowie;
+			this.Stamina = stamina;
+			this.MaxStamina = stamina;
+			this.Obrazenia = obrazenia;
+			this.MaxZdrowie = zdrowie;
 		}
 	}
 }
