fc0853ad9aeb56f7db96a8951c8321921c69df2f
Jakub Olejnik <jacobole2000@gmail.com>
2025-12-15 11:06:47 +0100

    First code

--- gra_rpg_konsola/Program.cs
+++ gra_rpg_konsola/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
 
+        static void Main(string[] args)
+        {
+            Console.Write("Hello world");
+        }
+    
+}
