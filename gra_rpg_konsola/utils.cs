d2253b94419b03273f8c51b139fccbeb890670a9
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2025-12-16 23:07:10 +0100

    Function to print 2 images (ASCII) next to each other

--- gra_rpg_konsola/utils.cs
+++ gra_rpg_konsola/utils.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,12 +11,88 @@ namespace gra_rpg_konsola
     {
         public static List<Enemy> ENEMIES = new List<Enemy>
             {
-                new Enemy("Zombie", 21.37f, 67.32f),
-                new Enemy("Skeleton", 34.12f, 58.44f),
-                new Enemy("Ghoul", 12.89f, 73.05f),
-                new Enemy("Vampire", 45.76f, 22.18f),
-                new Enemy("Demon", 66.03f, 41.97f),
-                new Enemy("Spider", 28.55f, 90.21f),
+                new Enemy("Zombie", 21.37f, 67.32f, @"
+    .-""""-.
+   / -   -  \
+  |  .-. .- |
+  |  \o| |o (
+  \     ^    \
+  |'.  )--'  /|
+ / / '-. .-'`\ \
+/ /'---` `---'\ \
+'.__.       .__.'
+    `|     |`
+     |     \
+     \      '--.
+      '.        `\
+        `'---.   |
+           ,__) /
+            `..'
+"),
+                new Enemy("Skeleton", 34.12f, 58.44f, @"
+    .-.
+   (o.o)
+    |=|
+   __|__
+ //.=|=.\\
+// .=|=. \\
+\\ .=|=. //
+ \\(_=_)//
+  (:| |:)
+   || ||
+   () ()
+   || ||
+   || ||
+  ==' '==
+"),
+                new Enemy("Gryphon", 12.89f, 73.05f, @"
+   ____       ____
+  /    )     (    \
+ /    (  ^_^  )    \
+|  {   \('v')/   }  |
+|   {   /   \   }   |
+|_)(   /\   /\   )(_|
+|)  (_ | \|/  |_)  (|
+'     ""--^^^^--""  '
+"),
+                new Enemy("Devil", 45.76f, 22.18f, @"
+   ,    ,    /\   /\
+  /( /\ )\  _\ \_/ /_
+  |\_||_/| < \_   _/ >
+  \______/  \|0   0|/
+    _\/_   _(_  ^  _)_
+   ( () ) /`\|V""""""V|/`\
+     {}   \  \_____/  /
+     ()   /\   )=(   /\
+     {}  /  \_/\=/\_/  \"),
+                new Enemy("Demon", 66.03f, 41.97f,@"
+         __        _
+       _/  \    _(\(o
+      /     \  /  _  ^^^o
+     /   !   \/  ! '!!!v'
+    !  !  \ _' ( \____
+    ! . \ _!\   \===^\)
+     \ \_!  / __!
+      \!   /    \
+(\_      _/   _\ )
+ \ ^^--^^ __-^ /(__
+  ^^----^^    ""^--v'
+"),
+                new Enemy("Spider", 28.55f, 90.21f,@"
+         (
+          )
+         (
+   /\  .-""""""-.  /\
+  //\\/  ,,,  \//\\
+  |/\| ,;;;;;, |/\|
+  //\\\;-""""""-;///\\
+ //  \/   .   \/  \\
+//`__\.-.-./__`\\
+// /.-(() ())-.\ \\
+(\ |)   '---'   (| /)
+ ` (|           |) `
+   \)           (/
+"),
             };
 
         public static List<Item> ITEMS = new List<Item>
@@ -31,5 +108,21 @@ namespace gra_rpg_konsola
                 new Item("Amulet Życia", 0, 0, 30, 0),
                 new Item("Pierścień Wytrzymałości", 0, 0, 0, 25)
             };
+
+        public static void printTextIn2Cols(string l, string r, int colWidth = 35)
+        {
+            string[] leftLines = l.Split('\n');
+            string[] rightLines = r.Split('\n');
+
+            for (int i = 0; i < Math.Max(leftLines.Length, rightLines.Length); i++)
+            {
+                string left = i < leftLines.Length ? leftLines[i].Trim('\r') : "";
+                string right = i < rightLines.Length ? rightLines[i].Trim('\r') : "";
+
+
+                Console.Write(left.PadRight(colWidth));
+                Console.WriteLine(right);
+            }
+        }
     }
 }
