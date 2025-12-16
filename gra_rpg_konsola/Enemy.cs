2e2e55525f6785bc32f661de1e0730fb4a9f03e5
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2025-12-16 23:07:40 +0100

    Added images ASCII to enemies

--- gra_rpg_konsola/Enemy.cs
+++ gra_rpg_konsola/Enemy.cs
@@ -12,13 +12,16 @@ namespace gra_rpg_konsola
 
         public float damage { get; private set; }
 
+        public string image { get; private set; }
 
-        public Enemy(string name, float health, float damage)
+
+        public Enemy(string name, float health, float damage, string image)
         {
             this.name = name;
             this.health = health;
             this.maxHealth = health;
             this.damage = damage;
+            this.image = image;
         }
 
         public void TakeDamage(float damage)
