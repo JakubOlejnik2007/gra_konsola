748ad96a6ea73e9b81a0ad33620979cc9ae4fe32
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2025-12-16 22:06:35 +0100

    Extended methods in Enemy class

--- gra_rpg_konsola/Enemy.cs
+++ gra_rpg_konsola/Enemy.cs
@@ -5,16 +5,28 @@ namespace gra_rpg_konsola
 
     public class Enemy
     {
-        public string name;
-        public float health;
-        public float damage;
+        public string name { get; }
+
+        public float health { get; private set; }
+        public float maxHealth { get;}
+
+        public float damage { get; private set; }
 
 
         public Enemy(string name, float health, float damage)
         {
             this.name = name;
             this.health = health;
+            this.maxHealth = health;
             this.damage = damage;
         }
+
+        public void TakeDamage(float damage)
+        {
+            health -= damage;
+        }
+
+        public bool isAlive() { return health > 0; }
+
     }
 }
